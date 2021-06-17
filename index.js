87b1ab1826d9e191961fb63ef0cf97cf573db71d
Sebastian Matthew Leal <sebastianmatthewleal@Sebastians-MacBook-Pro.local>
2021-06-17 16:57:34 -0700

    done with people

--- index.js
+++ index.js
@@ -42,7 +42,22 @@ class Airplane {
   */
   
  class Person {
-    
+    constructor(name, age){
+      this.name = name;
+      this.age = age; 
+      this.stomach = [];
+    }
+    eat(food){
+      if(this.stomach.length < 10){
+        return this.stomach.push(food)
+      }
+    }
+    poop(){
+      return this.stomach = []
+    }
+    toString(){
+      return `${this.name}, ${this.age}`
+    }
   }
   
   /*
